656498894560cc1e3a9bdc644448204dd5048c78
tgzhafri <tgzhafri@gmail.com>
2021-10-06 16:10:58 +0800

    update layout add lights when pressed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var key = document.querySelectorAll(".key");
 var keyList = [];
+var light = document.querySelectorAll(".light");
 
 var sound1 = document.getElementById("sound1");
 var sound2 = document.getElementById("sound2");
@@ -21,6 +22,16 @@ var key7 = document.getElementById("key7");
 var key8 = document.getElementById("key8");
 var key9 = document.getElementById("key9");
 
+var light1 = document.getElementById("light1");
+var light2 = document.getElementById("light2");
+var light3 = document.getElementById("light3");
+var light4 = document.getElementById("light4");
+var light5 = document.getElementById("light5");
+var light6 = document.getElementById("light6");
+var light7 = document.getElementById("light7");
+var light8 = document.getElementById("light8");
+var light9 = document.getElementById("light9");
+
 window.addEventListener("keydown", keyPressed);
 window.addEventListener("keyup", keyReleased);
 
@@ -31,47 +42,55 @@ function keyPressed (evt) {
         console.log("key 1 pressed");
         sound1.play();
         key1.classList.add("pressed");
-  
+        light1.classList.add("lightup");
     }
     if(keyList[50]) {
         console.log("key 2 pressed");
         sound2.play();
         key2.classList.add("pressed");
+        light2.classList.add("lightup");
     }
     if(keyList[51]) {
         console.log("key 3 pressed");
         sound3.play();
         key3.classList.add("pressed");
+        light3.classList.add("lightup");
     }
     if(keyList[52]) {
         console.log("key 4 pressed");
         sound4.play();
         key4.classList.add("pressed");
+        light4.classList.add("lightup");
     }
     if(keyList[53]) {
         console.log("key 5 pressed");
         sound5.play();
         key5.classList.add("pressed");
+        light5.classList.add("lightup");
     }
     if(keyList[54]) {
         console.log("key 6 pressed");
         sound6.play();
         key6.classList.add("pressed");
+        light6.classList.add("lightup");
     }
     if(keyList[55]) {
         console.log("key 7 pressed");
         sound7.play();
         key7.classList.add("pressed");
+        light7.classList.add("lightup");
     }
     if(keyList[56]) {
         console.log("key 8 pressed");
         sound8.play();
         key8.classList.add("pressed");
+        light8.classList.add("lightup");
     }
     if(keyList[57]) {
         console.log("key 9 pressed");
         sound9.play();
         key9.classList.add("pressed");
+        light9.classList.add("lightup");
     }
 }
 function keyReleased (evt) {
@@ -85,68 +104,16 @@ function keyReleased (evt) {
     key7.classList.remove("pressed");
     key8.classList.remove("pressed");
     key9.classList.remove("pressed");
+
+    light1.classList.remove("lightup");
+    light2.classList.remove("lightup");
+    light3.classList.remove("lightup");
+    light4.classList.remove("lightup");
+    light5.classList.remove("lightup");
+    light6.classList.remove("lightup");
+    light7.classList.remove("lightup");
+    light8.classList.remove("lightup");
+    light9.classList.remove("lightup");
 }
 
-
-// slider function
-
-var sliderHolder = document.getElementById('slider-holder');
-
-var sliderItem = [
-    {
-        bgImg: "url(images/img-1.jpg)",
-        // title: "Let's go on Vacation!",
-    },
-    {
-        bgImg: "url(images/img-2.jpg)",
-        // title: "Let's go on Vacation!",
-    },
-    {
-        bgImg: "url(images/img-3.jpg)",
-        // title: "Let's go on Vacation!",
-    },
-    {
-        bgImg: "url(images/img-4.jpg)",
-        // title: "Let's go on Vacation!",
-    },
-    {
-        bgImg: "url(images/img-5.jpg)",
-        // title: "Let's go on Vacation!",
-    },
-]
-
-sliderHolder.style.width = sliderItem.length * 100 + "vw";
-
-for(var i = 0; i < sliderItem.length; i++) {
-    var sliderEle = document.createElement('div');
-    // var sliderTitle = document.createElement('h1');
-    // sliderTitle.innerHTML = sliderItem[i].title;
-
-    sliderEle.classList.add('slider');
-    // sliderEle.appendChild(sliderTitle);
-
-    sliderEle.style.backgroundImage = sliderItem[i].bgImg;
-    sliderHolder.appendChild(sliderEle);
-}
-
-var pos = 0;
-var sliderTimerID = null;
-sliderTimerID = setInterval(sliderTimer, 3000);
-
-function sliderTimer() {
-    pos-= 100;
-    if(pos < -((sliderItem.length-1)* 100)) {
-        pos = 0;
-    }
-    sliderHolder.style.transform = `translate(${pos}vw)`;
-}
-
-function stopTimer() {
-    clearInterval(sliderTimerID);
-}
-
-function resumeTimer() {
-    console.log("mouse out")
-    sliderTimerID = setInterval(sliderTimer, 2000);
-}
 
